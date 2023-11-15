e8630d6e02bced89e20906e620551d27e53ac78c
marcomadv <imarcoma@gmail.com>
2023-11-15 12:50:58 +0100

    cambios en celda principalrowwatch

--- Marvel_SwifUi/PrincipalView/Row/PrincipalRowViewWatch.swift
+++ Marvel_SwifUi/PrincipalView/Row/PrincipalRowViewWatch.swift
@@ -10,7 +10,7 @@ struct PrincipalRowViewWatch: View {
             AsyncImage(url: character.thumbnail?.urlPhoto) { photo in
                 photo
                     .resizable()
-                    .frame(width: 200, height: 280, alignment: .center)
+                    .frame(width: 172, height: 200, alignment: .center)
                     .cornerRadius(30)
                     .opacity(0.85)
                     .brightness(0.07)
@@ -21,14 +21,14 @@ struct PrincipalRowViewWatch: View {
             }
             .frame(width: 50, height: 50)
             Text(character.name ?? "")
-                .frame(width: 200, height: 50, alignment: .center)
+                .frame(width: 172, height: 50, alignment: .center)
                 .font(.title3).background(.ultraThinMaterial)
                 .fontWeight(.bold)
                 .multilineTextAlignment(.center)
                 .cornerRadius(30, corners: [.bottomLeft, .bottomRight])
                 .foregroundColor(.white)
                 .bold()
-                .padding([.top], 170)
+                .padding([.top], 150)
                 .id(1)
 #endif
         }
